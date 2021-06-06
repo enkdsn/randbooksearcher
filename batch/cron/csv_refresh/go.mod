c070b57eb9611d2311d6c058437a413c31f7f2ba
ryota enokido <ryota.enokido.224@gmail.com>
2021-06-06 15:24:46 +0900

    csvでキャッシュすることにした

---
+++ batch/cron/csv_refresh/go.mod
@@ -0,0 +1,5 @@
+module github.com/enkdsn/randbooksearcher/tsvrefresh
+
+go 1.13
+
+require github.com/enkdsn/randbooksearcher/core v0.0.0-20210606014537-b6eeafb60d87 // indirect
---
+++ batch/cron/csv_refresh/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+
+	"github.com/enkdsn/randbooksearcher/core/components/fetch"
+)
+
+func main() {
+	refreshOrEbookCSV()
+}
+
+func refreshOrEbookCSV() {
+	file, err := os.OpenFile("../../../resource/or_ebook.csv", os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	err = file.Truncate(0)
+
+	writer := csv.NewWriter(file)
+
+	oef := fetch.NewOrEbookFetcher()
+	bs, err := oef.Fetch()
+	for _, b := range bs {
+		writer.Write([]string{b.URL, b.Name, strconv.Itoa(b.Price), b.ISBN})
+	}
+	writer.Flush()
+
+	defer file.Close()
+}
